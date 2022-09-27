3fdcccfc19320d7ab7789b4fe6cfed36699b9ea9
brk-a <fnjakai@outlook.com>
2022-09-27 09:20:50 +0300

    bars, part 1: basics

---
+++ 0x01-learnD3/app.js
@@ -0,0 +1,24 @@
+const DUMMY_DATA = [
+    {id: 1, value: 10, region: 'RW'},
+    {id: 2, value: 6.18, region: 'TZ'},
+    {id: 3, value: 12.5, region: 'ZA'},
+    {id: 4, value: 13.31, region: 'KE'},
+    {id: 5, value: 9, region: 'UG'},
+    {id: 6, value: 8.5, region: 'NG'}
+]
+
+// d3.select('div')
+//     .selectAll('p')
+//     .data(DUMMY_DATA)
+//     .enter()
+//     .append('p')
+//     .text((data) => {return data.region});
+
+const container = d3.select('div')
+    .classed('container', true);
+
+const bars = container.selectAll('.bar')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('div')
+    .classed('.bar', true);
