df8e889253c836ed0e33cfc7c0c171b8f9426392
brk-a <fnjakai@outlook.com>
2022-09-29 10:30:25 +0300

    world map: skeleton

---
+++ 0x02-learnD3/app.js
@@ -0,0 +1,23 @@
+const width = 1000;
+const height = 618;
+
+const svg = d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+const projection = d3.geoMercator();
+const path = d3.geoPath(projection);
+
+const g = svg. append('g');
+
+ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+    .then((data) => {
+        const countries = topojson.feature(data.objects.countries);
+        g.selectAll('path')
+            .data(countries.features)
+            .enter()
+            .append('path')
+            .attr('class', 'country')
+            .attr('d', path);
+    });
