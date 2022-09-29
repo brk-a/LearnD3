4bf111ee8ae5122c8f5ec47d1eb8463371c9c305
brk-a <fnjakai@outlook.com>
2022-09-29 11:04:16 +0300

    world map: static, but works properly

--- 0x02-learnD3/app.js
+++ 0x02-learnD3/app.js
@@ -1,10 +1,11 @@
-const width = 650;
-const height = 402;
+const width = 1000;
+const height = 618;
 
 const svg = d3.select('body')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
+
 
 const projection = d3.geoMercator()
     .scale(140)
@@ -13,13 +14,13 @@ const path = d3.geoPath(projection);
 
 const g = svg.append('g');
 
- d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
-    .then((data) => {
-        const countries = topojson.feature(data, data.objects.countries);
-        g.selectAll('path')
-            .data(countries.features)
-            .enter()
-            .append('path')
-            .attr('class', 'country')
-            .attr('d', path);
-    });
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+.then((data) => {
+    const countries = topojson.feature(data, data.objects.countries);
+    g.selectAll('path')
+        .data(countries.features)
+        .enter()
+        .append('path')
+        .attr('class', 'country')
+        .attr('d', path);
+});
