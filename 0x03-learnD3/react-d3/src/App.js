2074453f17bfec5396a4c32f7c435bed565b07e9
brk-a <fnjakai@outlook.com>
2022-10-03 17:03:00 +0000

    re-factor code

--- 0x03-learnD3/react-d3/src/App.js
+++ 0x03-learnD3/react-d3/src/App.js
@@ -1,10 +1,41 @@
 // import logo from './logo.svg';
-// import './App.css';
-import React from 'react';
+import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import {select} from 'd3';
 
 function App() {
+  const [data, setData] = useState([25,38,45,68,20]);
+  const svgRef = useRef();
+
+  useEffect(() => {
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    svg.select('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('class', 'updated')
+      .attr('r', value => value)
+      .attr('cx', value =>  value * 2)
+      .attr('cy', value =>  value * 2)
+      .attr('stroke', 'red');
+    console.log(svg);
+  }, [data]);
+  
+  const handleFilter = (data) => {
+    return(setData(data.map(value => value + 5)));
+  }
+
+  const handleUpdate = (data) => {
+    return(setData(data.map(value => value <= 35)));
+  }
+
   return (
-    <svg className='App'></svg>
+    <div className='container'>
+      <svg className='App' ref={svgRef}></svg>
+      <button onClick={handleUpdate}> Update Data </button>
+      <button onClick={handleFilter}> Filter Data </button>
+    </div>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
