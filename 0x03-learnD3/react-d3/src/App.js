c9e0e5531bd3062514f64f6310bbdcb23e8603c5
brk-a <fnjakai@outlook.com>
2022-10-06 13:36:03 +0000

    manually animated bar chart

--- 0x03-learnD3/react-d3/src/App.js
+++ 0x03-learnD3/react-d3/src/App.js
@@ -1,25 +1,32 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useRef, useState } from 'react';
-import {select, line, curveNatural, axisBottom, scaleLinear, axisRight} from 'd3';
+import {select, axisBottom, scaleLinear, axisRight, scaleBand} from 'd3';
 
 function App() {
   const [data, setData] = useState([25, 38, 45, 68, 20, 65, 75]);
   const svgRef = useRef();
 
-  const xScale = scaleLinear()
-    .domain([0, data.length - 1])
-    .range([0, 300]);
+  const xScale = scaleBand()
+    .domain(data.map((_, index) => index))
+    .range([0, 300])
+    .padding(0.5);
   
   const yScale = scaleLinear()
     .domain([0, 185.5])
     .range([185.5, 0]);
 
+  const colourScale = scaleLinear()
+  .domain([92.75, 139.125, 185.5])
+  .range(['green', 'orange', 'red'])
+  .clamp(true);
+
   useEffect(() => {
     console.log(svgRef);
     const svg = select(svgRef.current);
   
-    const xAxis = axisBottom(xScale);
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length);
     svg.selectAll('.x-axis')
       .style('transform', 'translateY(185.5px)')
       .call(xAxis);
@@ -29,10 +36,10 @@ function App() {
       .style('transform', 'translateX(300px)')
       .call(yAxis);
 
-    const myLine = line()
-      .x((value, index) => {return (xScale(index))})
-      .y(yScale)
-      .curve(curveNatural);
+    // const myLine = line()
+    //   .x((value, index) => {return (xScale(index))})
+    //   .y(yScale)
+    //   .curve(curveNatural);
     // svg.selectAll('path')
     //   .data([data])
     //   .enter()
@@ -42,25 +49,37 @@ function App() {
     //   .attr('fill', 'none')
     //   .attr('stroke', 'blue');
 
-    svg.selectAll('.line')
-      .data([data])
-      .join('path')
-      .attr('class', 'line')
-      .attr('d', value => myLine(value))
-      .attr('fill', 'none')
-      .attr('stroke', 'blue');
+    // svg.selectAll('.line')
+    //   .data([data])
+    //   .join('path')
+    //   .attr('class', 'line')
+    //   .attr('d', value => myLine(value))
+    //   .attr('fill', 'none')
+    //   .attr('stroke', 'blue');
+
+    svg.selectAll('bar')
+      .data(data)
+      .join('rect')
+      .attr('class', 'bar')
+      .style('transform', 'scale(1, -1)')
+      .attr('x', (_, index) => xScale(index))
+      .attr('y', -185.5)
+      .attr('width', xScale.bandwidth())
+      .transition()
+      .attr('fill', colourScale)
+      .attr('height', value => 185.5 - yScale(value));
 
     console.log(svg);
-  }, [data, xScale, yScale]);
+  }, [data, xScale, yScale, colourScale]);
   
   console.log(svgRef.current);
 
   const handleFilter = () => {
-    return(setData(data.map(value => value + 5)));
+    return(setData(data.map(value => value <= 45)));
   }
 
   const handleUpdate = () => {
-    return(setData(data.map(value => value <= 35)));
+    return(setData(data.map(value => value + 15)));
   }
 
   return (
@@ -68,7 +87,7 @@ function App() {
       <svg className='App' ref={svgRef}>
         <g className='x-axis'></g>
         <g className='y-axis'></g>
-        <path d="M0,150 100,100 150,120" stroke="blue" fill="none"/>
+        {/* <path d="M0,150 100,100 150,120" stroke="blue" fill="none"/> */}
       </svg>
       <button onClick={handleUpdate}> Update Data </button>
       <button onClick={handleFilter}> Filter Data </button>
