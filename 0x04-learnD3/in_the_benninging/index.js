3b30739ff74f06729ced3411b162380d989e4216
brk-a <fnjakai@outlook.com>
2022-11-17 03:16:34 +0000

    make a face using react: part 2

--- 0x04-learnD3/in_the_benninging/index.js
+++ 0x04-learnD3/in_the_benninging/index.js
@@ -5,8 +5,44 @@ import ReactDOM from 'react-dom'
 // console.log(ReactRouterDOM)
 // console.log(ReactDOM)
 
+const width = 960
+const height = 500
+const centreX = width / 2
+const centreY =  height / 2
+const strokeWidth = 10
+const eyeOffetX = 90
+const eyeOffsetY = 180
+const eyeRadius = 50
+
 const App = () => (
-    <h1>Testing JSX...</h1>
+    <svg width={width} height={height}>
+        <circle 
+            r={centreY - strokeWidth / 2}
+            cx={centreX} 
+            cy={centreY} 
+            fill="yellow" 
+            stroke="black" 
+            stroke-width="10"
+        />
+        <circle 
+            r={eyeRadius}
+            cx={centreX - eyeOffetX} 
+            cy={centreY - eyeOffsetY}
+            fill="black"
+        />
+        <circle 
+            r={eyeRadius}
+            cx={centreX + eyeOffetX} 
+            cy={centreY - eyeOffsetY}
+            fill="black"
+        />
+        <path 
+            d="M 360 300 C 422.5 400, 547.5 400, 610 300" 
+            stroke="black" 
+            stroke-width='30' 
+            fill="transparent"
+        />
+    </svg>
 )
 
 const rootElement = document.getElementById('root')
