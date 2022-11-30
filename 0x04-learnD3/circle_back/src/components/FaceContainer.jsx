084d780a4669159e02c098cb69ae7a2fcca8987f
brk-a <fnjakai@outlook.com>
2022-11-30 02:09:50 +0000

    optimise: `useCallback` set to fire when`setMousePosition` is registered

--- 0x04-learnD3/circle_back/src/components/FaceContainer.jsx
+++ 0x04-learnD3/circle_back/src/components/FaceContainer.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
+import React, {useState, useCallback} from 'react'
 
 const FaceContainer = ({children, width, height, initialMousePosition}) => {
 
   const [mousePosition, setMousePosition] = useState(initialMousePosition)
 
-  const handleMouseMove = (e) => {
+  const handleMouseMove = useCallback((e) => {
     const {clientX, clientY} = e
     // setMousePosition ({x: clientX, y: clientY})
     return(setMousePosition ({x: clientX, y: clientY}))
-  }
+  }, [setMousePosition])
 
   return (
     <svg width={width} height={height} onMouseMove={handleMouseMove} mousePosition={mousePosition}>
