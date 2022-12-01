3eaccf1472a6d401d33db87e13f6b27db5553cc1
brk-a <fnjakai@outlook.com>
2022-12-01 11:27:23 +0000

    refactor

--- 0x04-learnD3/circle_back/src/components/BackgroundCircle.jsx
+++ 0x04-learnD3/circle_back/src/components/BackgroundCircle.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState, useCallback}from 'react'
 
-const BackgroundCircle = ({circleY, circleRadius, circleX, mousePosition}) => {
+const BackgroundCircle = ({circleY, circleRadius, circleX, mousePosition, initialMousePosition}) => {
 
-    // const [mousePosition, setMousePosition] = useState()
-    // onMouseMove={handleMouseMove}
+    const [mousePosition, setMousePosition] = useState(initialMousePosition)
+
+    const handleMouseMove = useCallback((e) => {
+      const {clientX, clientY} = e
+      // setMousePosition ({x: clientX, y: clientY})
+      return(setMousePosition ({x: clientX, y: clientY}))
+    }, [setMousePosition])
 
     return (
         <>
@@ -11,7 +16,9 @@ const BackgroundCircle = ({circleY, circleRadius, circleX, mousePosition}) => {
                 r={circleRadius}
                 cx={mousePosition.x}
                 cy={mousePosition.y}
-                fill="yellow" 
+                fill="yellow"
+                onMouseMove={handleMouseMove}
+                mousePosition={mousePosition}
                 // stroke="black" 
                 // stroke-width="10"
             />
--- 0x04-learnD3/circle_back/src/components/FaceContainer.jsx
+++ 0x04-learnD3/circle_back/src/components/FaceContainer.jsx
@@ -1,17 +1,9 @@
-import React, {useState, useCallback} from 'react'
+import React from 'react'
 
-const FaceContainer = ({children, width, height, initialMousePosition}) => {
-
-  const [mousePosition, setMousePosition] = useState(initialMousePosition)
-
-  const handleMouseMove = useCallback((e) => {
-    const {clientX, clientY} = e
-    // setMousePosition ({x: clientX, y: clientY})
-    return(setMousePosition ({x: clientX, y: clientY}))
-  }, [setMousePosition])
+const FaceContainer = ({children, width, height}) => {
 
   return (
-    <svg width={width} height={height} onMouseMove={handleMouseMove} mousePosition={mousePosition}>
+    <svg width={width} height={height} >
         {/* <g transform={`translate(${circleX}, ${circleY})`}> */}
         <g>
             {children}
