a80053053ea9b11c8c7fd8d12b6f5b46b24c4f83
brk-a <fnjakai@outlook.com>
2022-12-02 12:17:19 +0000

    mouse move, again.

--- 0x04-learnD3/data_load/src/App.jsx
+++ 0x04-learnD3/data_load/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useCallback } from 'react'
+
+
+const width = 960
+const height = 500
+const circleRadius = 30
+const initialMousePosition = {x: width / 2, y: height / 2}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(initialMousePosition)
+
+  const handleMouseMove = useCallback((e) => {
+    const {clientX, clientY} = e
+    setData({x: clientX, y: clientY})
+  }, [setData])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <svg width={width} height={height} onMouseMove={handleMouseMove}>
+        <circle
+          cx={data.x}
+          cy={data.y}
+          r={circleRadius}
+        />
+      </svg>
+    </>
   )
 }
 
