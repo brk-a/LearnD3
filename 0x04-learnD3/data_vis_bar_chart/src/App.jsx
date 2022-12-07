4dc24d34d5b7140527cb32a2b6fb52c76c69c464
brk-a <fnjakai@outlook.com>
2022-12-07 02:26:01 +0000

    dodgy bar chart: part 2

--- 0x04-learnD3/data_vis_bar_chart/src/App.jsx
+++ 0x04-learnD3/data_vis_bar_chart/src/App.jsx
@@ -1,11 +1,34 @@
+import React, {useState, useEffect, useCallback} from "react"
 
+const csvUrl = 'https://gist.githubusercontent.com/brk-a/fd9b084a37cbeb52fb9852f675c70715/raw/cc3f426eb12d379543b726d8df8b2ecd371712e9/United-Nations-World-Population-Prospects-2019.csv'
+
+const width = 1280
+const height = 791
+const centreX = width / 2
+const centreY = height / 2
+
+const pieArc = d3.arc()
+  .innerRadius(0)
+  .outerRadius(width)
 
 function App() {
 
-  return (
-    <div>
+  const [data, setData] = useState(null)
 
-    </div>
+  useEffect(() => {
+    d3.csv(csvUrl).then(setData)
+  }, [])
+
+  if (!data) return <pre> Loading ... </pre>
+
+  return (
+    <svg>
+      <g transform={`translate(${centreX}, ${centreY})`}>
+        {data.map((d, i) => (
+          <div key={i}>{d}</div>
+        ))}
+      </g>
+    </svg>
   )
 }
 
