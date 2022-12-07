360ae25c0a7ecbbc51fa2feb8e63b9a58821d598
brk-a <fnjakai@outlook.com>
2022-12-07 03:19:55 +0000

    dodgy bar chart, part 3: bars w. padding, show on screen

--- 0x04-learnD3/data_vis_bar_chart/src/App.jsx
+++ 0x04-learnD3/data_vis_bar_chart/src/App.jsx
@@ -6,26 +6,41 @@ const width = 1280
 const height = 791
 const centreX = width / 2
 const centreY = height / 2
-
-const pieArc = d3.arc()
-  .innerRadius(0)
-  .outerRadius(width)
+const margin = {top: 20, right: 20, bottom: 20, left: 20} 
 
 function App() {
 
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    d3.csv(csvUrl).then(setData)
+    const row = d => {
+      d.Population = +d['2020']
+      return d
+    }
+
+    d3.csv(csvUrl, row).then(data => {
+      setData(data.slice(0, 20))
+    })
   }, [])
 
   if (!data) return <pre> Loading ... </pre>
 
+  const innerHeight = height - margin.top - margin.bottom
+  const innerWidth = width - margin.right - margin.left
+
+  const yScale = d3.scaleBand()
+    .domain(data.map(c => c.Country))
+    .range([0, innerHeight])
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, r => r.Population)])
+    .range([0, innerWidth])
+
   return (
-    <svg>
-      <g transform={`translate(${centreX}, ${centreY})`}>
+    <svg width={width} height={height}>
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
         {data.map((d, i) => (
-          <div key={i}>{d}</div>
+          <rect key={i} x={0} y={yScale(d.Country)} width={xScale(d.Population)} height={yScale.bandwidth()}/>
         ))}
       </g>
     </svg>
