5ecfe9216dd337a951fb377d2368999dddedf190
brk-a <fnjakai@outlook.com>
2022-12-12 12:39:57 +0000

    where to get data

--- 0x04-learnD3/data_vis_world_map/src/hooks/useData.js
+++ 0x04-learnD3/data_vis_world_map/src/hooks/useData.js
@@ -1,16 +1,18 @@
 import React, {useState, useEffect} from "react"
 
-const geoUrl = ''
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-10m.json'
 
 const useData = () => {
     const [data, setData] = useState(null)
-    const row = () => {
-        useEffect((d) => {
-            d.sth = sth
-        }, [])
-        return d
-    }
-    d3.sth(data, row).then(setData)
+    useEffect(() => {
+        d3.json(jsonUrl).then((topology) => {
+            const {countries, land} = topology.objects
+            setData({
+                land: d3.feature(topology, land),
+                interiors: d3.mesh(topology, countries, (a, b) => a !== b)
+            })
+        })
+    }, [])
     return data
 }
 
