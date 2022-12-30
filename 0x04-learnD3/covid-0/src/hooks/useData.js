e9f6deb2aa16976163deda6c060ac4014c43ae79
brk-a <fnjakai@outlook.com>
2022-12-30 11:00:38 +0000

    fetch data from repo

--- 0x04-learnD3/covid-0/src/hooks/useData.js
+++ 0x04-learnD3/covid-0/src/hooks/useData.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-const csvUrl=''
+const csvUrl='https://gist.githubusercontent.com/brk-a/b634cb05a8615241f130921e73575116/raw/a235d2536763790ed7fa72e092bcd8decc624909/covid-deaths.csv'
 
 export default useData = () => {
 
